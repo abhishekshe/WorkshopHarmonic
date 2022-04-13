12f94ad71af4b2d6736fe9002fbb751fae09969c
Abhishe <abhisheknarwariya813@gmail.com>
2022-04-13 11:06:00 +0530

    Workshop

---
+++ HarmonicDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HarmonicDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any value");
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N != 0)
+            {
+                double sum = 0;
+
+
+                for (double i = 1; i <= N; i++)
+                {
+
+                    sum += (1 / i);
+                }
+                Console.WriteLine(" harmonic no: is " + sum);
+
+
+            }
+            else
+            {
+                Console.WriteLine("zero is not a valid value try another value");
+            }
+
+        }
+
+    }
+}
